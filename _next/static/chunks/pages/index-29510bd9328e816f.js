656a5bceb92e47165b45e044672d17306d5ee00a
aprame <anandofreal@gmail.com>
2022-12-05 09:49:17 +0700

    Deploy gh-pages

--- _next/static/chunks/pages/index-29510bd9328e816f.js
+++ _next/static/chunks/pages/index-29510bd9328e816f.js
@@ -605,7 +605,7 @@ function Home() {
             // Update this to use the address (public key) for your zkApp account
             // To try it out, you can try this address for an example "Add" smart contract that we've deployed to 
             // Berkeley Testnet B62qqkb7hD1We6gEfrcqosKt9C398VLp1WXeTo1i9boPoqF7B1LxHg4
-            const zkAppAddress = "";
+            const zkAppAddress = "B62qrDe16LotjQhPRMwG12xZ8Yf5ES8ehNzZ25toJV28tE9FmeGq23A";
             // This should be removed once the zkAppAddress is updated.
             if (!zkAppAddress) {
                 console.error('The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Berkeley Testnet: B62qqkb7hD1We6gEfrcqosKt9C398VLp1WXeTo1i9boPoqF7B1LxHg4');
